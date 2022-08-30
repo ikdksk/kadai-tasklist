e450eee4491e408879ecc2370bb705cd3fe85ed8
kosuke.ikeda <ikdksk>
2022-08-30 12:27:07 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_122939) do
+ActiveRecord::Schema.define(version: 2022_08_30_121959) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
